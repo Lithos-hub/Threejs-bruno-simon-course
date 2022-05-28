d7524c215a69e2664fee21e62939e788475861c6
unknown <lithos.contact@gmail.com>
2022-05-28 08:55:47 +0200

    Added textures, used filters and texture position and rotation properties

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import gsap from "gsap";
+import * as dat from "dat.gui";
 
 const canvas = document.querySelector(".webgl");
 
@@ -34,13 +36,13 @@ window.addEventListener("resize", () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2)); // => If the device has a pixel ration over 2, we'll take 2
 });
 
-window.addEventListener('dblclick', () => {
-  if (!document.fullscreenElement) {
-    canvas.requestFullscreen();
-  } else {
-    document.exitFullscreen();
-  }
-})
+// window.addEventListener('dblclick', () => {
+//   if (!document.fullscreenElement) {
+//     canvas.requestFullscreen();
+//   } else {
+//     document.exitFullscreen();
+//   }
+// })
 
 // ? |||||||| SCENE & CAMERA ||||||||
 
@@ -53,7 +55,7 @@ const camera = new THREE.PerspectiveCamera(
   75, // => FOV
   sizes.width / sizes.height, // => Aspect ratio
   0.1, // => Near
-  100 // => Far
+  1000 // => Far
 );
 
 // const aspectRatio = sizes.width / sizes.height;
@@ -66,7 +68,8 @@ const camera = new THREE.PerspectiveCamera(
 //   100
 // );
 
-camera.position.setZ(3);
+camera.position.setZ(80);
+camera.position.setY(5);
 
 scene.add(camera);
 
@@ -78,6 +81,38 @@ controls.enableDamping = true;
 const axesHelper = new THREE.AxesHelper(1.5); // Length as parameter
 scene.add(axesHelper);
 
+// ? |||||||| TEXTURES ||||||||
+// const image = new Image()
+// const texture = new THREE.Texture(image);
+
+// image.onload = () => {
+//   texture.needsUpdate = true;
+// }
+// image.src = './img/textures/textureWood.jpg';
+const loadingManager = new THREE.LoadingManager();
+
+loadingManager.onStart = () => console.log('LOADING MANAGER - Start loading');
+loadingManager.onLoad = () => console.log('LOADING MANAGER - Loaded');
+loadingManager.onProgress = () => console.log('LOADING MANAGER - Progress');
+loadingManager.onError = () => console.log('LOADING MANAGER - Error');
+
+const textureLoader = new THREE.TextureLoader(loadingManager)
+const textureMoon = textureLoader.load('./img/textures/moon.jpg',
+  // () => console.log('Loading texture...'),
+  // () => console.log('Progress texture...'),
+  // () => console.log('Error when loading texture.'),
+  )
+const textureEarth = textureLoader.load('./img/textures/earth.jpg');
+const textureWood = textureLoader.load('./img/textures/wood.jpg');
+
+textureWood.repeat.x = 2;
+textureWood.repeat.y = 3;
+textureWood.wrapS = THREE.MirroredRepeatWrapping;
+textureWood.wrapT = THREE.MirroredRepeatWrapping;
+
+textureWood.offset.x = 0.5; // => Texture replacement to the right
+textureWood.rotation = 1;
+
 // ? |||||||| OBJECTS & MATERIALS & MESHES, ETC ||||||||
 
 // ** Group
@@ -85,44 +120,49 @@ const group = new THREE.Group();
 scene.add(group);
 
 // ** Objects
-// const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const geometryMoon = new THREE.SphereGeometry(1, 64, 64);
+const geometryEarth = new THREE.SphereGeometry(1, 64, 64);
+const geometryCube = new THREE.BoxGeometry(1, 1, 1);
 
-
-
-const geometry = new THREE.BufferGeometry();
-
-const count = 5000;
-const positionsArray = new Float32Array(count * 3 * 3);
-
-for (let i = 0; i <= count * 3 * 3; i++) {
-  positionsArray[i] = Math.random() - 0.5;
-}
-
-const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
-geometry.setAttribute('position', positionsAttribute);
+// const geometryMoon = new THREE.BuffergeometryMoon();
 
 // ** Materials
-const geoMaterial = new THREE.MeshBasicMaterial({
-  color: "#d63b3b",
-  wireframe: true,
+const materialMoon = new THREE.MeshBasicMaterial({
+  wireframe: false,
+  map: textureMoon,
+});
+const materialEarth = new THREE.MeshBasicMaterial({
+  wireframe: false,
+  map: textureEarth,
+});
+const materialCube = new THREE.MeshBasicMaterial({
+  wireframe: false,
+  map: textureWood,
 });
 
 // ** Meshes
-const geometryMesh = new THREE.Mesh(geometry, geoMaterial);
-scene.add(geometryMesh);
+const geometryMoonMesh = new THREE.Mesh(geometryMoon, materialMoon);
+const geometryEarthMesh = new THREE.Mesh(geometryEarth, materialEarth);
+const geometryCubeMesh = new THREE.Mesh(geometryCube, materialCube);
+scene.add(geometryMoonMesh);
+scene.add(geometryEarthMesh);
+scene.add(geometryCubeMesh);
 
 // ** Position
-geometryMesh.position.set(0, 0, 0); // X, Y, Z
+geometryMoonMesh.position.set(5, 0, -5); // X, Y, Z
+geometryEarthMesh.position.set(0, 0, 0); // X, Y, Z
 
 // ** Scale
-geometryMesh.scale.set(1, 1, 1); // X, Y, Z
+geometryMoonMesh.scale.set(1, 1, 1); // X, Y, Z
+geometryEarthMesh.scale.set(5, 5, 5); // X, Y, Z
 
 // ** Rotation
-// geometryMesh.rotation.reorder('YXZ');
-geometryMesh.rotation.set(0, 0, 0); // X, Y, Z
+// geometryMoonMesh.rotation.reorder('YXZ');
+geometryMoonMesh.rotation.set(0, 0, 0); // X, Y, Z
+geometryEarthMesh.rotation.set(0, 0, 0.1); // X, Y, Z
 
 // ** Camera controls
-// camera.lookAt(geometryMesh.position);
+// camera.lookAt(geometryMoonMesh.position);
 
 // ? |||||||| RENDER ||||||||
 
@@ -135,13 +175,16 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // Clock
 const clock = new THREE.Clock();
 
-// gsap.to(geometryMesh.position, {
+// gsap.to(geometryMoonMesh.position, {
 //   duration: 1,
 //   delay: 1,
 //   x: 2
 // });
 
 // ? |||||||| ANIMATIONS ||||||||
+
+let theta = 0;
+let dTheta = 2 * Math.PI / 1000;
 
 const animate = () => {
   const elapsedTime = clock.getElapsedTime();
@@ -154,14 +197,26 @@ const animate = () => {
   // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3;
   // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3;
   // camera.position.y = cursor.y * 5;
-  // camera.lookAt(geometryMesh.position);
+  // camera.lookAt(geometryMoonMesh.position);
 
   // Update objects
-  // geometryMesh.rotation.x = elapsedTime;
-  // geometryMesh.rotation.y = elapsedTime;
+  // geometryMoonMesh.rotation.x = elapsedTime;
+  geometryEarthMesh.rotation.y -= 0.01
+  theta += dTheta * 2;
+  geometryMoonMesh.rotation.x += 0.01;
+  geometryMoonMesh.position.x = 35 * Math.cos(theta);
+  geometryMoonMesh.position.y = 35 * Math.cos(theta) / 2;
+  geometryMoonMesh.position.z = 35 * Math.sin(theta);
   // camera.position.y = Math.sin(elapsedTime);
-  // camera.position.x = Math.cos(elapsedTime);
-  // camera.lookAt(geometryMesh.position);
+  // camera.position.y = Math.cos(elapsedTime);
+  // camera.lookAt(geometryEarthMesh.position);
+
+  // ! Important: Use textures with a width and height power of 2 (512 x 512, 1024 x 1024, etc.)
+
+  
+  textureEarth.generateMipmaps = false; // => Will not divide the texture to mipmaps. We'll gain at performance
+  textureEarth.minFilter = THREE.NearestFilter // => Used when the objects is very far. The texture will be sharper
+  textureEarth.magFilter = THREE.NearestFilter // => Used when the objects is very close or the texture image is very small. The texture will be sharper
 
   // Render
   renderer.render(scene, camera);
@@ -170,3 +225,28 @@ const animate = () => {
 };
 
 animate();
+
+// ? |||||||| DEBUG ||||||||
+const gui = new dat.GUI({ closed: true, width: 500 });
+gui.hide();
+
+const debugObject = {
+  color: "#d63b3b",
+  spin: () => gsap.to(geometryMoonMesh.rotation, { duration: 1, y: geometryMoonMesh.rotation.y + 10 }),
+}
+// => object and property to add, name of property, min, max and step
+gui.add(geometryMoonMesh.position, 'y', -3, 3, 0.01).name('Cube Y');
+gui.add(geometryMoonMesh.position, 'x', -3, 3, 0.01).name('Cube X');
+gui.add(geometryMoonMesh.position, 'z', -3, 3, 0.01).name('Cube Z');
+gui.add(geometryMoonMesh, 'visible').name('Cube visibility');
+gui.add(materialMoon, 'wireframe').name('Cube wireframe');
+
+gui.add(debugObject, 'spin').name('Cube spin');
+
+gui
+.addColor(debugObject, 'color')
+.name('Cube color')
+.onChange(() => materialMoon.color.set(debugObject.color));
+// gui.add(geometryMoonMesh.position, 'y').min(-3).max(3).step(0.01);
+// gui.add(geometryMoonMesh.position, 'x').min(-3).max(3).step(0.01);
+// gui.add(geometryMoonMesh.position, 'z').min(-3).max(3).step(0.01);
