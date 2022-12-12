fea194a2a813143b18dd753768d504d840daf38d
Carlos Segura <lithos.contact@gmail.com>
2022-12-12 20:58:02 +0100

    add cubemaps

--- main.js
+++ main.js
@@ -39,6 +39,10 @@ gltf.load("./static/models/kitchen/kitchen.glb", (model) => {
   
   // model.scene.scale.set(1, 1, 1);
   scene.add(model.scene);
+
+  model.scene.rotation.y = Math.PI * 1.5;
+
+  updateAllMaterials()
 });
 
 // ? |||||||| SIZES ||||||||
@@ -79,13 +83,13 @@ const scene = new THREE.Scene();
 // ** Cameras
 
 const camera = new THREE.PerspectiveCamera(
-  75, // => FOV
+  35, // => FOV
   sizes.width / sizes.height, // => Aspect ratio
   0.1, // => Near
   1000 // => Far
 );
 
-camera.position.set(5, 5, 7);
+camera.position.set(6, 8, 8);
 
 scene.add(camera);
 
@@ -94,16 +98,37 @@ const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 // ** Axes helper
-const axesHelper = new THREE.AxesHelper(1.5); // Length as parameter
+const axesHelper = new THREE.AxesHelper(5); // Length as parameter
 scene.add(axesHelper);
+
+// ? |||||||| TEXTURE ||||||||
+
+const environmentMap = new THREE.CubeTextureLoader().load([
+  "./static/cubemap/px.png",
+  "./static/cubemap/nx.png",
+  "./static/cubemap/py.png",
+  "./static/cubemap/ny.png",
+  "./static/cubemap/pz.png",
+  "./static/cubemap/nz.png",
+]);
+
 
 // ? |||||||| LIGHTS ||||||||
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+// const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
-directionalLight.position.set(-4, 4, 0);
+directionalLight.position.set(-3, 2, 0);
 
-scene.add(ambientLight, directionalLight);
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(0, 3, 0);
+
+scene.add(directionalLight, pointLight);
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+  directionalLight,
+  1
+);
+scene.add(directionalLightHelper);
 
 // ? |||||||| TEXTURES ||||||||
 
@@ -136,6 +161,17 @@ scene.add(floorMesh);
 // ** Rotation
 
 // ** Camera controls
+
+
+// ? |||||||| UPDATE MATERIALS ||||||||
+const updateAllMaterials = () => {
+  scene.traverse((child) => {
+    if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+      child.material.envMap = environmentMap
+      child.material.envMapIntensity = 1.25
+    }
+  })
+}
 
 // ? |||||||| RENDER ||||||||
 
@@ -176,3 +212,7 @@ animate();
 
 // ? |||||||| GUI ||||||||
 const gui = new dat.GUI();
+const debugObject = {}
+
+gui.add(directionalLight.position, "x", -10, 10, 0.001).name("Dir. Light Position X")
+gui.add(directionalLight, 'intensity', 0, 10, 0.001).name("Dir. Light Intensity")
